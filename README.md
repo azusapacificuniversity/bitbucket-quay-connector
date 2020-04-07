7905ea296d5c0175021ae22bd027471e8d697e4e
Brian Monroe <paradoxguitarist@fedoraproject.org>
2020-04-07 10:02:45 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# bitbucket-quay-connector
-Middleware for Bitbucket and Quay webhooks built with Appsody.
+# Bitbucket Quay Connector
+## Overview
+This software provides middleware for self hosted instances of
+[Bitbucket](https://bitbucket.org), Atlassian's version control repository, to
+make webhook calls to [Quay](https://access.redhat.com/products/red-hat-quay),
+RedHat's container image registry. This project is built with
+[appsody](https://appsody.dev/docs).
+## Configuration
+You need to configure the config.json with the name of the Bitbucket repo (key)
+and the URL that quay needs you to post to as the value. The webhook will throw
+an error if this is not configured properly.
+
+You should also note that when using the "test"  button while setting up the
+webhook from Bitbucket, do not include testing the quay server link in the
+config.json, or the required secret configuration.
+
+### Environmental Variables
+These need to be set when your final appsody container is deployed.
+
+`SERVER_SECRET` Required - Also needs to be set in your Bitbucket webhook.
+
+`QUAY_CA_FILE` Optional - If Quay's root CA is not publically signed, you'll
+need to set this to the name of the cert.pem that you've added to the root of
+this project. (example: QUAY_CA_FILE=ExampleRootCA.pem)
+
+### Building the Appsody Project:
+You can read all about building and deploying Appsody projects
+[here](https://appsody.dev/docs/using-appsody/building-and-deploying/), but for
+the scope of this project we'll only cover generating and obtaining a
+container image for use with whatever container system you use.  
+Here are the steps you'll take:
+- [Install Appsody](https://appsody.dev/docs/installing/installing-appsody)
+- Clone this repository, and change it to your current working directory.
+- Create a new repository in quay and add a
+[Custom Git Repository Push Build Trigger](https://docs.quay.io/guides/custom-trigger.html). Copy the Webhook Endpoint URL.
+- Edit the `config.json` to have your Bitbucket repo name and the Webhook
+Endpoint URL you created in the previous step.
+- If you need to trust a Root CA, copy it to the repository root directory.
+- Run `appsody build` - This builds a container image that you can push.
+
+### Configuring Bitbucket
+You'll need to add a webhook that points to your newly built container image,
+either with the FQDN or the IP address and then add the `/api` Endpoint.
+Example: https://192.168.1.101:3000/api or
+https://bitbucket-quay-connector.cloudapps.master.ose/api After that be sure to
+add the string you set for the `SERVER_SECRET`.
+
+You will also need to add the
+SSH Public Key that Quay recreated to your repo as part of  the
+[Custom Git Repository Push Build Trigger](https://docs.quay.io/guides/custom-trigger.html).
---
+++ app.js
@@ -0,0 +1,107 @@
+module.exports = (/*options*/) => {
+  // Set some Variables.
+  const express = require('express');
+  const app = express();
+  const bodyParser = require('body-parser');
+  const config = require('./config.json');
+  const request = require('request');
+  const crypto = require('crypto');
+  const fs = require('fs');
+  app.use(bodyParser.json());
+  // Check if we're using a custom cert.
+  if (process.env.QUAY_CA_FILE){
+    const cert = fs.readFileSync(`/project/user-app/${process.env.QUAY_CA_FILE}`)
+    console.log(`Loaded custom cert for Quay: ${process.env.QUAY_CA_FILE}`)
+  } else {
+    console.log('No custom certs found.')
+  };
+  // Verification function to check if it is actually Bitbucket who is POSTing here
+  const verifyBitbucket = (req) => {
+    if (!req.headers['user-agent'].includes('Bitbucket')) {
+      return false;
+    }
+    // Compare their hmac signature to our hmac signature
+    // (hmac = hash-based message authentication code)
+    const theirSignature = req.headers['x-hub-signature'];
+    if (theirSignature == null) {
+      return false;
+    }
+    const payload = JSON.stringify(req.body);
+    const secret = process.env.SERVER_SECRET
+    const ourSignature = `sha256=${crypto.createHmac('sha256', secret).update(payload).digest('hex')}`;
+    return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+  };
+  // Function for if we're not Authorized.
+  const notAuthorized = (req, res) => {
+    console.log('The Secret Key was wrong or missing.');
+    res.writeHead(401, { 'Content-Type': 'text/plain' });
+    res.end('HMAC signature was missing or incorrect.');
+  };
+
+  // This function POSTs to Quay.
+  const authorizationSuccessful = (payload) => {
+    console.log('Bitbucket has authenticated.');
+    // Build a new payload that quay can consume.
+    var quaypayload = {"commit":payload.changes[0].toHash,"ref":payload.changes[0].ref.id,"default_branch":payload.changes[0].ref.displayId};
+    // Make the POST request to the url in the config.
+    // Set different options if we're using our own CA.
+    if (process.env.QUAY_CA_FILE){
+      var quayoptions = {
+        json: quaypayload,
+        agentOptions: {
+          ca: fs.readFileSync(`/project/user-app/${process.env.QUAY_CA_FILE}`)
+        }
+      }
+    } else {
+      json: quayoptions
+    }
+    request.post(config[payload.repository.name], quayoptions, (error, res, body) => {
+      if (error) {
+        // Log errors if they exist.
+        console.error(error)
+        return
+      }
+      // Log the results.
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(body)
+    })
+  };
+
+  // Set up the URI endpoints we can use:
+  app.get('/', (req, res) => {
+    // We use this for a readyness probe.
+    req.log.info({message: 'Container is live.'});
+    res.send('Container is live.');
+  });
+
+  app.post('/api', (req, res) => {
+    if (req.headers['x-event-key'] == 'diagnostics:ping') {
+      // This is a test and bitbucket doesn't send the HMAC, so we need to respond early.
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('pong - Please note that the server secret was not tested as a part of this test.');
+    } else {
+      if (verifyBitbucket(req)) {
+        // Bitbucket called with the right secret
+        // Pull the payload out of the request.
+        var payload = req.body
+        // See if the config has an entry for the POSTing repository.
+        if (config.hasOwnProperty(payload.repository.name)){
+          // We know where to direct the QUAY payload.
+          authorizationSuccessful(payload);
+          // Quay was already updated. Respond to Bitbucket.
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Thanks Bitbucket <3');
+        } else {
+          // Error because we don't have this repo in the config.
+          res.writeHead(400, { 'Content-Type': 'text/plain' });
+          res.end(`${payload.repository.name} has not been added to the config. Please see the README.md on how to configure access for this repo.`);
+        }
+      } else {
+        // Someone else calling
+        notAuthorized(req, res);
+      }
+    }
+  });
+
+  return app;
+};
